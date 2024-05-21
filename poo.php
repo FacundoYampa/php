ead35b15576732982afaea267682689b0b57118b
FacundoYampa <facundoyampa@gmail.com>
2024-05-21 16:18:36 -0300

    funciones y clases

--- poo.php
+++ poo.php
@@ -8,12 +8,30 @@
 // printVar(5);
 // printVar("texto");
 
-function printInt(int $myNum){ /*le implicamos un tipado, ya no se puede*/
-    echo $myNum."\n";
-}
+/*le implicamos un tipado, ya no se puede*/
+// function printInt(int $myNum){ 
+//     echo $myNum."\n";
+// }
 
-printInt(5);
-printInt(10.5);       /*Deprecated: Implicit conversion from float 10.5 to int loses precision*/
-printInt("texto");    /*Fatal error: Uncaught TypeError: printInt(): Argument #1 ($myNum) must be of type int, string given,*/
+// printInt(5);
+// printInt(10.5);       /*Deprecated: Implicit conversion from float 10.5 to int loses precision*/
+// printInt("texto");    /*Fatal error: Uncaught TypeError: printInt(): Argument #1 ($myNum) must be of type int, string given,*/
 
 //----------------------- CLASES
+
+class MyClass{ #creo una clase
+    public $name; #parámetros, public: para poder acceder a ellos
+    public $age;
+
+    function __construct($name, $age){ #funcion constructora
+        $this->name=$name;
+        $this->age=$age; #asignamos variables a la clase
+    }
+}
+
+$myNewClass = new MyClass("Facu",24); #instanciamos la clase
+
+print_r($myNewClass);
+
+// acceder al parametro
+echo $myNewClass->name . "\n";
